f4bb6329621a425ea6d5f1a2046aaa800ed7189a
madhurangar <mail.pvgm@gmail.com>
2021-08-16 12:21:25 +1000

    minor update

--- pcopy.py
+++ pcopy.py
@@ -5,9 +5,9 @@ from utils import utils as u
 
 ap = argparse.ArgumentParser(description="Copy pre defined list of camera RAW files to a new folder. See more at <https://github.com/madhurangar/photo-copier>")
 ap.add_argument('input', type=str, help='Text file containing the selected files list to be coppied over')
-ap.add_argument('-filetypes', type=dict, help='Raw photos file extension (default: crw)', default={'.NEF', '.ARW', '.CR2', '.CR3', '.DNG', '.CRW'})
-ap.add_argument('-force-copy', type=bool, help='Overwrite files already in the selected-photos folder', default=False)
 ap.add_argument('-folder-name', type=str, help='Folder (name) to copy files over', default="selected_photos")
+ap.add_argument('-force-copy', type=bool, help='Overwrite files already in the selected-photos folder', default=False)
+ap.add_argument('-filetypes', type=dict, help='Raw photo file extensions (default: crw)', default={'.NEF', '.ARW', '.CR2', '.CR3', '.DNG', '.CRW'})
 args = ap.parse_args()
 
 cwd = u.get_cwd()
