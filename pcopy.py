2e555e2affcba48b6e573b1ad77918b2a6fa20e4
madhurangar <mail.pvgm@gmail.com>
2021-08-16 12:12:27 +1000

    add some intelligence

--- pcopy.py
+++ pcopy.py
@@ -3,16 +3,21 @@
 import argparse
 from utils import utils as u
 
-PHOTOS_FOLDER = "selected_photos" # folder to copy new photos over 
-
 ap = argparse.ArgumentParser(description="Copy pre defined list of camera RAW files to a new folder. See more at <https://github.com/madhurangar/photo-copier>")
 ap.add_argument('input', type=str, help='Text file containing the selected files list to be coppied over')
-ap.add_argument('-filetype', type=str, help='Raw photos file extension (default: crw)', default="CRW")
+ap.add_argument('-filetypes', type=dict, help='Raw photos file extension (default: crw)', default={'.NEF', '.ARW', '.CR2', '.CR3', '.DNG', '.CRW'})
+ap.add_argument('-force-copy', type=bool, help='Overwrite files already in the selected-photos folder', default=False)
+ap.add_argument('-folder-name', type=str, help='Folder (name) to copy files over', default="selected_photos")
 args = ap.parse_args()
 
 cwd = u.get_cwd()
-extension = args.filetype
 filenames = u.read_file(args.input)
 
-filepaths_list = u.create_files_list(filenames, extension, cwd, PHOTOS_FOLDER)
-u.copy_files(filepaths_list,cwd, PHOTOS_FOLDER)
+extensions = args.filetypes
+force_copy = args.force_copy
+photos_folder = args.folder_name
+
+
+filepaths = u.create_files_list(filenames, extensions, cwd, photos_folder )
+u.copy_files(filepaths,cwd, photos_folder, force_copy)
+
