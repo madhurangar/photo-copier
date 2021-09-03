46857ec63528c17977323a747a290f91528ca5a9
madhurangar <mail.pvgm@gmail.com>
2021-09-03 10:32:14 +1000

    fix import

--- pcopy.py
+++ pcopy.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3 
 
 import argparse
-from utils import utils as u
+import utils.utils as u
 
-ap = argparse.ArgumentParser(description="Copy pre defined list of camera RAW files to a new folder. See more at <https://github.com/madhurangar/photo-copier>")
-ap.add_argument('input', type=str, help='Text file containing the selected files list to be coppied over')
-ap.add_argument('-n', '--folder-name', type=str, help='Folder (name) to copy files over', default="selected_photos")
-ap.add_argument('-f', '--force-copy', help='Overwrite files already in the selected-photos folder', action='store_true')
-ap.add_argument('-t', '--filetypes', nargs='+', type=str, help='File extensions (defaults: NEF ARW CR2 CR3 DNG and CRW)', default=['.NEF', '.ARW', '.CR2', '.CR3', '.DNG', '.CRW'])
-ap.add_argument('-s', '--separate-chars', nargs='+', type=str, help='Split file name to construct the original file name. e.g. extract "IMG12345" from "IMG12345-copy_new (1).ext" (defaults: . - and _ )', default=['.', '-', '_'])
+ap = argparse.ArgumentParser(description="Copy list of camera RAW files into a new folder. See more at <https://github.com/madhurangar/photo-copier>")
+ap.add_argument('input', type=str, help='Text file containing the filenames (with or without file extensions)')
+ap.add_argument('-n', '--folder-name', type=str, help='Folder (name! not the path) to copy files over', default="selected_photos")
+ap.add_argument('-f', '--force-copy', help='Overwrite already coppied files', action='store_true')
+ap.add_argument('-t', '--filetypes', nargs='+', type=str, help='Raw file extensions (defaults: NEF ARW CR2 CR3 DNG and CRW)', default=['.NEF', '.ARW', '.CR2', '.CR3', '.DNG', '.CRW'])
+ap.add_argument('-s', '--separate-chars', nargs='+', type=str, help='Split chars to construct the original RAW file name. e.g. extract "IMG12345" from "IMG12345-copy_new (1).ext" (defaults: . - and _ )', default=['.', '-', '_'])
 args = ap.parse_args()
 
 cwd = u.get_cwd()
